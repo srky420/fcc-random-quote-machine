077f731974b36080af8b5c9bc68b2851b26ed4b2
srky <srky420@gmail.com>
2023-11-14 19:13:55 +0500

    Create links and optimizations

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -25,24 +25,26 @@ class Presentational extends React.Component {
         this.props.submitQuoteRequest();
     }
     render() {
+        // Color styles from props
         const textStyles = {
             color: this.props.color
         }
         const buttonStyles = {
             backgroundColor: this.props.color
         }
-        document.querySelector('body').style.backgroundColor = this.props.color; 
+        document.querySelector('body').style.backgroundColor = this.props.color;
         return (
             <div className='quote-box' id='quote-box'>
-                <p className={this.props.fetching ? 'quote fadeOut' : 'quote fadeIn'} id='text' style={textStyles}><i className='fa fa-quote-left'></i> { this.props.quote.content }</p>
+                <p className={this.props.fetching ? 'quote fadeOut' : 'quote fadeIn'} id='text' style={textStyles}><i className='fa fa-quote-left'></i> { this.props.quote.content } <i className='fa fa-quote-right'></i></p>
                 <p className={this.props.fetching ? 'author fadeOut' : 'author fadeIn'} id='author' style={textStyles}>- { this.props.quote.author }</p>
                 <div className='btn-div'>
                     <div className='socials'>
-                        <a className='link' style={textStyles} href='https://www.twitter.com/intent/tweet'><i className='fa fa-2x fa-twitter'></i></a>
-                        <a className='link' style={textStyles} href='twitter.com/intent/tweet'><i className='fa fa-2x fa-facebook'></i></a>
+                        <a className='link' id='' style={buttonStyles} href='https://www.twitter.com/intent/tweet' rel='noreferrer' target='_blank'><div className='overlay'></div><i className='fa fa-twitter'></i></a>
                     </div>
-                    <button id='new-quote' className='btn' style={buttonStyles} onClick={this.handleQuoteRequest} disabled={this.props.fetching}>Next Quote</button>
+                    <button id='new-quote' className='btn' style={buttonStyles} onClick={this.handleQuoteRequest} disabled={this.props.fetching}><div className='overlay'></div>Next Quote</button>
                 </div>
+                <p className='creator'>By <a href='https://www.linkedin.com/in/shahrukh-khan-2b8968242/' target='_blank' rel='noreferrer'>Shahrukh</a></p>
+                <p className='created'>Created in React and Redux</p>
             </div>
         );
     }
